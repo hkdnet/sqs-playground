6a9bdbc21080dc83c7607ee13ea932d69526f63b
hkdnet <satoko.itse@gmail.com>
2018-09-20 16:55:33 +0900

    concume and produce

--- consumer/main.go
+++ consumer/main.go
@@ -10,10 +10,22 @@ import (
 const sqsURL = "http://sqs:9324"
 const queueName = "q"
 
+type MessageViewer struct{}
+
+func (v MessageViewer) Process(msg gateway.Message) error {
+	fmt.Printf("%s: %s\n", msg.MessageID, msg.Body)
+	return nil
+}
+
 func main() {
-	_, err := gateway.NewClient(queueName, gateway.NewLocalSessionCreator(sqsURL))
+	client, err := gateway.NewClient(queueName, gateway.NewLocalSessionCreator(sqsURL))
 	if err != nil {
-		log.Fatalf("new client: %s", err)
+		log.Fatalf("new client: %s\n", err)
 	}
-	fmt.Println("ok")
+
+	err = client.ReceiveMessage(MessageViewer{})
+	if err != nil {
+		log.Fatalf("recv message: %s\n", err)
+	}
+	log.Println("done")
 }
--- producer/main.go
+++ producer/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"time"
+
+	"github.com/hkdnet/sqs-playground/gateway"
+)
+
+const sqsURL = "http://sqs:9324"
+const queueName = "q"
 
 func main() {
-	fmt.Println("producer")
+	client, err := gateway.NewClient(queueName, gateway.NewLocalSessionCreator(sqsURL))
+	if err != nil {
+		log.Fatalf("new client: %s\n", err)
+	}
+
+	msg := time.Now().Format("15:04:05")
+
+	err = client.SendMessage(msg)
+	if err != nil {
+		log.Fatalf("send message: %s\n", err)
+	}
+	log.Println("Successfully sent a message.")
 }
