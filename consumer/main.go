0dfae4993a1adfa260fa79650a2f538aac7a69de
hkdnet <satoko.itse@gmail.com>
2018-09-23 16:37:31 +0900

    Use context

--- consumer/main.go
+++ consumer/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"time"
 
 	"github.com/hkdnet/sqs-playground/gateway"
+	"github.com/pkg/errors"
 )
 
 const sqsURL = "http://sqs:9324"
@@ -13,11 +15,23 @@ const queueName = "q"
 
 type MessageViewer struct {
 	client *gateway.SQSClient
+
+	ctx context.Context
 }
 
 func (v *MessageViewer) Process(msg gateway.Message) error {
-	fmt.Printf("%s: %s\n", msg.MessageID, msg.Body)
-	return v.client.DeleteMessage(msg.ReceiptHandle)
+	ch := make(chan error)
+	go func() {
+		fmt.Printf("%s: %s\n", msg.MessageID, msg.Body)
+		ch <- v.client.DeleteMessage(msg.ReceiptHandle)
+	}()
+
+	select {
+	case <-v.ctx.Done():
+		return errors.Wrap(v.ctx.Err(), msg.MessageID)
+	case err := <-ch:
+		return err
+	}
 }
 
 func main() {
@@ -26,13 +40,21 @@ func main() {
 		log.Fatalf("new client: %s\n", err)
 	}
 
-	viewer := &MessageViewer{client: client}
 	for {
-		err = client.ReceiveMessage(viewer)
-		if err != nil {
-			log.Fatalf("recv message: %s\n", err)
-		}
+		func() {
+			limit := time.After(5 * time.Second)
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+			viewer := &MessageViewer{
+				client: client,
+				ctx:    ctx,
+			}
+			err = client.ReceiveMessage(viewer)
+			if err != nil {
+				log.Fatalf("recv message: %s\n", err)
+			}
 
-		time.Sleep(5 * time.Second)
+			<-limit
+		}()
 	}
 }
