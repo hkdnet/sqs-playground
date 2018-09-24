53a3c1d5c9805ce7d8ecc2ed72f5473b86966dfc
hkdnet <satoko.itse@gmail.com>
2018-09-24 18:55:05 +0900

    Set VisibilityTimeout

--- gateway/client.go
+++ gateway/client.go
@@ -40,6 +40,7 @@ func (c *SQSClient) ReceiveMessage(processor MessageProcessor) error {
 	resp, err := c.svc.ReceiveMessage(&sqs.ReceiveMessageInput{
 		QueueUrl:            c.queueURL,
 		MaxNumberOfMessages: aws.Int64(1),
+		VisibilityTimeout:   aws.Int64(10), // sec
 		WaitTimeSeconds:     aws.Int64(20), // sec
 	})
 
